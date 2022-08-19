9b0ad4b67ddba706dcbac71fa395e555f9dd4422
nmhieu2201 <minhhieudz.22012000a@gmail.com>
2022-08-19 21:06:11 +0700

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+// function handleScroll() {
+//   var scroll = window.scrollY;
+//   var headerEl = document.getElementById("header");
+//   // console.log(headerEl);
+//   if (scroll >= 400) {
+//     headerEl.classList.add("scroll-header");
+//     // console.log("run");
+//   } else {
+//     headerEl.classList.remove("scroll-header");
+//   }
+  
+// }
+// function handleCourse() {
+//   var selectEle = document.getElementById("language-select");
+//   var selectList = document.getElementById("select-list");
+//   selectList.setAttribute("style","display: block;opacity: 1");
+  
+// }
+console.log(screen.height);
